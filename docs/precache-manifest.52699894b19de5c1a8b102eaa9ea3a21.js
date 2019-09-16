166d002aa6484d0f8d4150a1177a7eeacd916162
shihcheng <tunnysushi@gmail.com>
2019-09-16 23:16:32 +0800

    add week 1

---
+++ docs/precache-manifest.52699894b19de5c1a8b102eaa9ea3a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c71d103c2fb9288793c0a3f38290a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d08290d2c47d003b66d8",
+    "url": "./static/css/main.a548889e.chunk.css"
+  },
+  {
+    "revision": "47d48b697d0f2e6db262",
+    "url": "./static/js/2.e51a09a2.chunk.js"
+  },
+  {
+    "revision": "d08290d2c47d003b66d8",
+    "url": "./static/js/main.017e9b9b.chunk.js"
+  },
+  {
+    "revision": "7989cd6719518eb16d3b",
+    "url": "./static/js/runtime~main.9f56a9c7.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// TODO: reacthook -> typescript
+import React, { PureComponent as Component } from 'react';
+import cx from 'classnames'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from  './App.module.css';
+
+import menu from './menu';
+
+const today = () => {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+
+  return yyyy + '/' + mm + '/' + dd
 }
 
-export default App;
+const CACHE_PREFIX = 'cache_'
+const CACHE = CACHE_PREFIX + today()
+
+const amendSum = (sum, price) => {
+  return sum + price
+}
+
+const amendCounter = (counter, item, num) => {
+  const newCounter = Object.assign({}, counter)
+
+  if (!newCounter[item]) {
+    newCounter[item] = 0
+  }
+
+  newCounter[item] = newCounter[item] + num
+
+  return newCounter
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    // @ counter 為當前項目數量
+    // @ sum 為所有項目總和金額
+    // 
+    // state: {
+    //  counter: {
+    //    "chicken": 0,
+    //    "beef": 1,
+    //  },
+    //  sum: 100
+    // }
+    this.state = {
+      counter: {},
+      sum: 0,
+    }
+
+    if (localStorage.hasOwnProperty(CACHE)) {
+      const cache = localStorage.getItem(CACHE)
+      this.state = JSON.parse(cache)
+    }
+
+    this.Count = this.Count.bind(this)
+  }
+
+  componentDidUpdate(preProps, preState) {
+    if (this.state !== preState) {
+      localStorage.setItem(CACHE, JSON.stringify(this.state))
+    } 
+  }
+
+  Count = (item, num, price) => {
+    const { counter, sum } = this.state
+
+    const isSumEqualZero = sum === 0
+    const isPriceNegative = price < 0 
+    const isNumberNegative = num === -1
+    const isItemEqualZero = counter && ( counter[item] === 0 || counter[item] === undefined)
+
+    if ((isSumEqualZero && isPriceNegative) || (isNumberNegative && isItemEqualZero)) {
+      return
+    }
+
+    this.setState({
+      counter: amendCounter(counter, item, num),
+      sum: amendSum(sum, price)
+    })
+  }
+
+  render() {
+    const { counter, sum } = this.state
+
+    return (
+      <div>
+        <div className={styles.container}>
+          <p>{ today() } <span className={styles.sum}>總金額: {sum} 元</span></p>
+        </div>
+        <div className={styles.menuContainer}>
+          {
+            menu.list.map((v, i) => {
+              return (
+                <div key={i}>
+                  <div className={styles.container} >
+                    <p className={cx(styles.box, styles.item)}>{ v.item }</p>
+                    <p className={styles.box}>{ v.price }</p>
+                    <p className={styles.box}>目前數量 { counter[i] || 0 }</p>
+                  </div>
+                  <div className={styles.container} key={i}>
+                    <p 
+                      className={cx(styles.box, styles.btn)}
+                      onClick={ e => {
+                        this.Count(i, -1, -v.price)
+                      }} 
+                    >-</p>
+                    <p 
+                      className={cx(styles.box, styles.btn)}
+                      onClick={ e => {
+                        this.Count(i, 1, v.price)
+                      }} 
+                    >+</p>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
