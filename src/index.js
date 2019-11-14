38ca95fc8af9ee9d45e5869adf86f379ce8a8577
Peter <peter0187@mtopv.com>
2019-11-14 18:23:17 +0800

    browser router to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import './index.css';
@@ -10,11 +10,11 @@ import * as serviceWorker from './serviceWorker';
 import storageStore from './store'
 
 render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={storageStore}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
